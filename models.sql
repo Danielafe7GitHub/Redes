10699a8b2cc53b5412b302268d8f0bf74d1e7905
Kareliavs <karelia.vilca@ucsp.edu.pe>
2017-12-09 13:50:45 -0500

    modelos sql sinonimos

--- models.sql
+++ models.sql
@@ -6,3 +6,11 @@ CREATE TABLE palabras
 );
 
 INSERT INTO palabras (palabra, referencia) VALUES ('gato', 'malo');
+
+CREATE TABLE sinonimos (
+    palabra            text,
+    sinonimo        text[]
+);
+INSERT INTO sinonimos (palabra, sinonimo) VALUES 
+('Gato','{"Felino","Garabato", "Michi", "Felix"}'),
+('Perro','{"Can","Canino", "Cachorro", "Sabueso"}'); 
