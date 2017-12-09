c55e32aabae58485eb9316fd8d9928f5c19ba0f2
Daniel Diaz <ework.diaz@gmail.com>
2017-12-09 13:57:14 -0500

    Indentation.

--- models.sql
+++ models.sql
@@ -7,10 +7,12 @@ CREATE TABLE palabras
 
 INSERT INTO palabras (palabra, referencia) VALUES ('gato', 'malo');
 
-CREATE TABLE sinonimos (
+CREATE TABLE sinonimos
+(
     palabra            text,
     sinonimo        text[]
 );
+
 INSERT INTO sinonimos (palabra, sinonimo) VALUES 
 ('Gato','{"Felino","Garabato", "Michi", "Felix"}'),
 ('Perro','{"Can","Canino", "Cachorro", "Sabueso"}'); 
