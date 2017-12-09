180897806f793ce26a7995cd0ba9a3c74c53c12a
Sergio <segio.rolando@gmail.com>
2017-12-09 18:27:20 -0500

    added addons

---
+++ addons/cargarSinonimos.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include </usr/include/postgresql/libpq-fe.h>
+
+using namespace std;
+
+//COMPILAR: g++ cargarSinonimos.cc -o e -Wall -I/usr/local/pgsql/include -L/usr/local/pgsql/lib -lpq
+
+#define RUTAARCHIVO "/home/sergio/Redes/Trabajo Final/Data/cleanSinonimo.txt"
+
+PGconn *cnn = NULL;
+PGresult *result = NULL;
+
+string host = "localhost";
+string port = "5432";
+string dataBase = "redes";
+string user = "redes";
+string passwd = "redes";
+
+int find(string principal,char letra)
+{
+    for(unsigned int i=0;i<principal.size();i++)
+    {
+        if(principal[i]==letra)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool findInVector(vector<string>vector,string palabra)
+{
+    for(unsigned int i=0;i<vector.size();i++)
+    {
+        if(vector[i]==palabra)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    vector<string>lPalabras;
+    vector<string>lSinonimos;
+    ifstream toRead;
+    toRead.open(RUTAARCHIVO);
+    if(!toRead.is_open())
+    {
+        cout<<"no se pudo abrir el fichero.."<<endl;
+        return 0;
+    }
+    string linea, palabra, sinonimos;
+    sinonimos="{\"";
+    bool flag=0;
+    while( !toRead.eof() )
+    {
+        getline(toRead,linea);
+        if(linea.size()!=0)
+        {
+            int pos = find(linea,',');
+            palabra=linea.substr(0,pos);
+            if(findInVector(lPalabras,palabra))
+            {
+                flag=1;
+                sinonimos += linea.substr(pos+1,linea.size()-1) + "\",\"";       
+            }else{
+                if(flag)
+                {
+                    lSinonimos.push_back(sinonimos.substr(0,sinonimos.size()-2)+"}");
+                    sinonimos="{\"";
+                    flag=0;
+                }
+                sinonimos += linea.substr(pos+1,linea.size()-1) + "\",\"";          
+                lPalabras.push_back(palabra);
+            }
+        }
+    }
+    lSinonimos.push_back(sinonimos.substr(0,sinonimos.size()-2)+"}");
+    toRead.close();
+
+    cnn = PQsetdbLogin(host.c_str(),port.c_str(),NULL,NULL,dataBase.c_str(),user.c_str(),passwd.c_str());
+
+    if (PQstatus(cnn) != CONNECTION_BAD) 
+    {
+        string instruccion;
+        for(unsigned int i=0;i<lPalabras.size();i++)
+        {
+            instruccion = "INSERT INTO sinonimos (palabra, sinonimos) VALUES('" + lPalabras[i] + "','" + lSinonimos[i] + "')";
+            result = PQexec(cnn,instruccion.c_str());
+
+            // Ahora nos toca liberar la memoria
+            PQclear(result);
+        }
+    }else{
+        cout<<"Error de Coneccion"<<endl;
+    }
+
+    return 0;
+}
